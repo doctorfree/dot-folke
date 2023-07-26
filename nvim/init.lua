2b3536bdcb9a275b7e8fa7cdc8c3327599fb0ff0
Ronald Record <github@ronrecord.com>
2023-07-26 15:00:10 -0700

    No need to import flash.nvim anymore

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,26 +15,7 @@ _G.dd = function(...)
 end
 vim.print = _G.dd
 
--- require("util.profiler").start()
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "LazyVimStarted",
-  callback = function()
-    vim.schedule(function()
-      -- require("util.profiler").stop()
-    end)
-  end,
-})
-
 -- vim.g.profile_loaders = true
 require("config.lazy")
 
 -- require("util.dashboard").setup()
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "VeryLazy",
-  callback = function()
-    require("config.env")
-    require("util").version()
-  end,
-})
